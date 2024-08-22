72a938d9b0863759d3fc46a92d661823272c39a5
mohm33443 <hassanelgohary@std.mans.edu.eg>
2024-08-22 15:19:03 +0300

    Add files via upload

---
+++ task2js.js
@@ -0,0 +1,30 @@
+let x =3;
+if(x=>3){
+    console.log('successfull');
+
+}else{
+    console.log('your questtion is wrong');
+
+}
+
+let u ='user';
+let a ='admin';
+switch(u){
+    case "user":
+        console.log("not admin found")
+        break;
+
+        case "admin":
+            console.log("you have all access");
+            break;
+}
+let arr =[1,2,3,4,5];
+let arrname=['mohamed','hassan','ali','mohaa','mona','elsyed','siad','fried']
+let obj={
+    name:'ali',
+    age :20
+};
+arr 
+for(let i=0; i>=6;i++){
+    console.log(i);
+}
